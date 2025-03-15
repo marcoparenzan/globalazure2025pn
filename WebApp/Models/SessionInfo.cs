2376bed4234f949f6aaa6666e60d89d5fdcab9dc
Marco Parenzan <marco.parenzan@1nn0va.it>
2025-03-15 11:59:08 +0100

    First Commit

---
+++ WebApp/Models/SessionInfo.cs
@@ -0,0 +1,116 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models;
+public partial class RoomDescription
+{
+    [JsonPropertyName("date")]
+    public DateTimeOffset Date { get; set; }
+
+    [JsonPropertyName("isDefault")]
+    public bool IsDefault { get; set; }
+
+    [JsonPropertyName("rooms")]
+    public List<RoomDescriptionRoom> Rooms { get; set; }
+
+    [JsonPropertyName("timeSlots")]
+    public List<TimeSlot> TimeSlots { get; set; }
+}
+
+public partial class RoomDescriptionRoom
+{
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("sessions")]
+    public List<Session> Sessions { get; set; }
+
+    [JsonPropertyName("hasOnlyPlenumSessions")]
+    public bool HasOnlyPlenumSessions { get; set; }
+}
+
+public partial class Session
+{
+    [JsonPropertyName("id")]
+    public object Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("startsAt")]
+    public DateTimeOffset StartsAt { get; set; }
+
+    [JsonPropertyName("endsAt")]
+    public DateTimeOffset EndsAt { get; set; }
+
+    [JsonPropertyName("isServiceSession")]
+    public bool IsServiceSession { get; set; }
+
+    [JsonPropertyName("isPlenumSession")]
+    public bool IsPlenumSession { get; set; }
+
+    [JsonPropertyName("speakers")]
+    public List<Speaker> Speakers { get; set; }
+
+    [JsonPropertyName("categories")]
+    public List<object> Categories { get; set; }
+
+    [JsonPropertyName("roomId")]
+    public long RoomId { get; set; }
+
+    [JsonPropertyName("room")]
+    public string Room { get; set; }
+
+    [JsonPropertyName("liveUrl")]
+    public object LiveUrl { get; set; }
+
+    [JsonPropertyName("recordingUrl")]
+    public object RecordingUrl { get; set; }
+
+    [JsonPropertyName("status")]
+    public string Status { get; set; }
+
+    [JsonPropertyName("isInformed")]
+    public bool IsInformed { get; set; }
+
+    [JsonPropertyName("isConfirmed")]
+    public bool IsConfirmed { get; set; }
+}
+
+public partial class Speaker
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
+
+public partial class TimeSlot
+{
+    [JsonPropertyName("slotStart")]
+    public string SlotStart { get; set; }
+
+    [JsonPropertyName("rooms")]
+    public List<TimeSlotRoom> Rooms { get; set; }
+}
+
+public partial class TimeSlotRoom
+{
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("session")]
+    public Session Session { get; set; }
+
+    [JsonPropertyName("index")]
+    public long Index { get; set; }
+}
---
+++ WebApp/Models/SpeakerInfo.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models;
+
+public partial class SpeakerInfo
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("firstName")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("lastName")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("fullName")]
+    public string FullName { get; set; }
+
+    [JsonPropertyName("bio")]
+    public string Bio { get; set; }
+
+    [JsonPropertyName("tagLine")]
+    public string TagLine { get; set; }
+
+    [JsonPropertyName("profilePicture")]
+    public Uri ProfilePicture { get; set; }
+
+    [JsonPropertyName("sessions")]
+    public List<SpeakerSession> Sessions { get; set; }
+
+    [JsonPropertyName("isTopSpeaker")]
+    public bool IsTopSpeaker { get; set; }
+
+    [JsonPropertyName("links")]
+    public List<Link> Links { get; set; }
+
+    [JsonPropertyName("questionAnswers")]
+    public List<object> QuestionAnswers { get; set; }
+
+    [JsonPropertyName("categories")]
+    public List<object> Categories { get; set; }
+
+
+    public string SpeakerUrl()
+    {
+        var bio = this.Links.SingleOrDefault(xx => xx.LinkType == "Sessionize");
+        if (bio is null) bio = this.Links.SingleOrDefault(xx => xx.LinkType == "Linkedin");
+        if (bio is null) bio = this.Links.FirstOrDefault();
+        if (bio is null) return string.Empty;
+        return bio.Url;
+    }
+
+}
+
+public partial class Link
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("linkType")]
+    public string LinkType { get; set; }
+}
+
+public enum LinkType { Blog, Facebook, Instagram, LinkedIn, Sessionize, Twitter, XTwitter };
+
+public partial class SpeakerSession
+{
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
